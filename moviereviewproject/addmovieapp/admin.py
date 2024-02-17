b23c17d8347cac809e13e0e57a5de02db11399a4
anju k praveen <anjukpraveen@gmail.com>
2024-02-17 23:09:26 +0530

    added

---
+++ moviereviewproject/addmovieapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . models import Movie
+from .models import User
+
+# Register your models here.
+admin.site.register(Movie)
+
+# Register your models here.
+#
+
+
